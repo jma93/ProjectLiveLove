d259c7cbae3d9f7f8353c02ac1ed9f05e08f6d1d
Jeffrey Ma <jma93@gatech.edu>
2017-09-18 23:36:13 -0400

    changed db connection for local host

--- php/dbConnection.php
+++ php/dbConnection.php
@@ -5,10 +5,19 @@
  * Date: 4/11/2017
  * Time: 7:09 PM
  */
-$DB_USER = 'mylivelo_epicintentions';
-$DB_PASSWORD = '5nmkQRr3v3WqIerKsJqt';
-$DB_HOST = 'mylivelove.com';
+// original database connection
+// $DB_USER = 'mylivelo_epicintentions';
+// $DB_PASSWORD = '5nmkQRr3v3WqIerKsJqt';
+// $DB_HOST = 'mylivelove.com';
+// $DB_NAME = 'mylivelo_mylivelove';
+// $dbc = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+
+//database to make the server run locally
+$DB_USER = 'root';
+$DB_PASSWORD = '';
+$DB_HOST = 'localhost';
 $DB_NAME = 'mylivelo_mylivelove';
 $dbc = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+
 
 ?>
