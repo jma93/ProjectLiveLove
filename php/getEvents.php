4595b14c9b160101c0c7221b96758bfce48d19b7
Jeffrey Ma <jma93@gatech.edu>
2017-11-15 00:39:43 -0500

    more ui stuff

--- php/getEvents.php
+++ php/getEvents.php
@@ -1,26 +1,45 @@
 <?php
-require 'dbConnection.php';
+// require 'dbConnection.php';
 
-$sql = "SELECT name, hostCompany, eventDescription
-        FROM event";
+// $sql = "SELECT name, hostCompany, eventDescription
+//         FROM event";
 
-$result = $dbc->query($sql);
+// $result = $dbc->query($sql);
 
-$arr = array();
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        $event = array(
-            "Event_Name" => $row["name"],
-            "Organization_Name" => $row["hostCompany"],
-            "Event_description" => $row["eventDescription"]
-        );
-        array_push($arr, $event);
-    }
-} else {
-    echo "0 results";
-}
-$dbc->close();
+$arr = array(
+    array(
+        "Event_Name" => "Furniture Bank of Metro Atlanta",
+        "Organization_Name" => "Animal Shelter",
+        "Event_description" => "Deliver furniture to needy families"
+    ),
+    array(
+        "Event_Name" => "Gateway Center",
+        "Organization_Name" => "Animal Shelter",
+        "Event_description" => "Give out food and resources to the poor"
+    ),
+    array(
+        "Event_Name" => "ReStart3:20:",
+        "Organization_Name" => "Animal Shelter",
+        "Event_description" => "Help redesignate spaces as shelter for the homeless"
+    )
+);
+echo json_encode($arr);
+
+// $arr = array();
+// if ($result->num_rows > 0) {
+//     // output data of each row
+//     while($row = $result->fetch_assoc()) {
+//         $event = array(
+//             "Event_Name" => $row["name"],
+//             "Organization_Name" => $row["hostCompany"],
+//             "Event_description" => $row["eventDescription"]
+//         );
+//         array_push($arr, $event);
+//     }
+// } else {
+//     echo "0 results";
+// }
+// $dbc->close();
 
 //$arr = array(
 //    array(
@@ -41,5 +60,5 @@ $dbc->close();
 //    )
 //);
 
-echo json_encode($arr);
+//echo json_encode($arr);
 ?>
